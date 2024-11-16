61a8c6b8ab4c61e08f75900e54b9fdca1a358508
Dwita Yolanda Margareth Saragih <dwitayolandamargarethsaragih@Dwitas-MacBook-Air.local>
2024-11-16 11:46:25 +0700

    project setup

---
+++ src/main/java/com/java_e_wallet/e_wallet_ledger_service/EWalletLedgerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.java_e_wallet.e_wallet_ledger_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalletLedgerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalletLedgerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_e_wallet/e_wallet_ledger_service/EWalletLedgerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_e_wallet.e_wallet_ledger_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletLedgerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
